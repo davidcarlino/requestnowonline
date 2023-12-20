4c81fbf54b3513b4ce24cabb6977e5050111c51c
davidcarlino <david.carlino20@gmail.com>
2023-12-20 12:08:43 +1100

    add hero and buttons

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import './Navbar.css';
 import icon from '../../assets/images/navbar_icon.png';
+import 
 
-const Navbar = () => {
+function Navbar = () => {
   return (
     <nav>
+      <a class="navbar-logo" href="/">
       <a href="/"><img alt='' src={icon}/></a>
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
+      </a>
+
+      <button
+      data-mdb-collapse-init
+      class="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarButtonsExample"
+      aria-controls="navbarButtonsExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarButtons">
+  
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Dashboard</a>
+        </li>
+      </ul>
+
+
+      <div class="d-flex align-items-center">
+        <button data-mdb-ripple-init type="button" class="buttonnavbar">
+          Login
+        </button>
+        <button data-mdb-ripple-init type="button" class="btn btn-primary me-3">
+          Sign up for free
+        </button>
+        <a
+          data-mdb-ripple-init
+          class="btn btn-dark px-3"
+          href="https://github.com/mdbootstrap/mdb-ui-kit"
+          role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+      </div>
+    </div>
     </nav>
   );
 };
+
+
 
 export default Navbar;
---
+++ frontend/src/components/buttons/buttonnavbar.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import "./buttonnavbar.css";
+import buttonnavbar from '../buttons/buttonnavbar';
+
+
+const LoadingButton = (props) => {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    function simulateNetworkRequest() {
+      return new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
+
+  return (
+    <div>
+        <button className='buttonnavbar' disabled={isLoading} onClick={!isLoading ? handleClick :null}>
+            {isLoading ? 'Loading…' : props.text}
+        </button>
+    </div>
+
+  );
+}
+
+export default LoadingButton;
